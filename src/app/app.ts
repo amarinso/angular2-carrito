8413670de1729ccf8e2251da1d5de197c2f4e90f
amarinso <amarinso@capgemini.com>
2016-01-06 22:52:02 +0100

    example of a simple todo list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,77 +1,44 @@
 /*
  * Angular 2 decorators and services
  */
-import {Component, EventEmitter, Input} from 'angular2/core';
+import { Component, EventEmitter, Input, View} from 'angular2/core';
+import {NgFor} from 'angular2/common';
 import {RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Http} from 'angular2/http';
 import {FORM_PROVIDERS} from 'angular2/common';
 
-import {Title} from './providers/title';
-import {XLarge} from './directives/x-large';
-import {Home} from './home/home';
-import {ProductSearch} from './search/productSearch';
-import {Cart} from './cart/cart';
 
 /*
  * App Component
  * Top Level Component
  */
 @Component({
-  // The selector is what angular internally uses
-  // for `document.querySelectorAll(selector)` in our index.html
-  // where, in this case, selector is the string 'app'
-  selector: 'app', // <app></app>
-  // We need to tell Angular's Dependency Injection which providers are in our app.
-  providers: [ FORM_PROVIDERS, Title],
-  // We need to tell Angular's compiler which directives are in our template.
-  // Doing so will allow Angular to attach our behavior to an element
-  directives: [ ROUTER_DIRECTIVES, XLarge, Cart  ],
-  // We need to tell Angular's compiler which custom pipes are in our template.
-  pipes: [],
-  // Our list of styles in our component. We may add more to compose many styles together
-  styles: [`
-    .title {
-      font-family: Arial, Helvetica, sans-serif;
-    }
-    main {
-      padding: 1em;
-    }
-
-  `],
-  // Every Angular template is first compiled by the browser before Angular runs it's compiler
-  template: `
-    <header>
-      <h1 class="title">Hello {{ title.value }}</h1>
-      <nav>
-        <a [routerLink]=" ['Home'] ">Home</a>
-        <a [routerLink]=" ['ProductSearch'] ">Surtido</a>
-      </nav>
-    </header>
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-
-    <footer x-large>
-      WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-    </footer>
-  `
+    selector: 'app'
 })
-@RouteConfig([
-  { path: '/', component: ProductSearch, as: 'ProductSearch' },
-  { path: '/cart/:code/:quantity', component: Cart, as: 'Cart' },
-  { path: '/home', component: Home, as: 'Home' }
-])
-export class App {
-  url: string = 'https://twitter.com/AngularClass';
-  constructor(public title: Title) {}
+@View({
+    template: `
+    <div>
+       <input placeholder="Todo" #todotext><br>
+        <button class="btn btn-block btn-success" (click)="addTodo(todotext)">Add Todo</button>
+        <ul>
+           <li *ngFor="#todo of todos">
+             {{ todo }}
+           </li>
+        </ul>
+    </div>
+    `,
+    directives: [NgFor]
+})
 
+export class App {
+    todos: string[] = ['First Todo', 'Second Todo'];
+
+    constructor() { }
+
+    addTodo( todo ) {
+        this.todos.push( todo.value );
+        todo.value = '';
+    }
 }
 
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- * or via chat on Gitter at https://gitter.im/AngularClass/angular2-webpack-starter
- */
+//bootstrap( App );
--- src/app/search/strategyResults.ts
+++
@@ -1,7 +0,0 @@
-import {Product} from '../providers/product';
-
-export interface StrategyResults {
-  quantity: number,
-  products: Product[],
-  error?: string
-}
